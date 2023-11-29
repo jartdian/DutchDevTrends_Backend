17da85e14ff901942f119e448a1f506db5b0216b
artdian <46709199+jartdian@users.noreply.github.com>
2023-11-29 22:01:59 +0100

    add mockup data for express

---
+++ data/citiesLangData.js
@@ -0,0 +1,33 @@
+const citiesLangData =
+{
+    "Rotterdam": {
+        "coordinates": [51.9225, 4.47917],
+        "data": {
+            "javascript": 1417,
+            "python": 1304,
+            "ruby": 32,
+            "java": 1336,
+            "c++": 2199,
+            "c#": 13006,
+            "go": 1268,
+            "swift": 106,
+            "php": 1075,
+        },
+    },
+    "Amsterdam": {
+        "coordinates": [52.3676, 4.9041],
+        "data": {
+            "javascript": 1500,
+            "python": 1575,
+            "ruby": 79,
+            "java": 1732,
+            "c++": 1976,
+            "c#": 13006,
+            "go": 2446,
+            "swift": 130,
+            "php": 955,
+        },
+    },
+}
+
+module.exports = citiesLangData;
