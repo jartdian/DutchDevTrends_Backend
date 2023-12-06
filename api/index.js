820472d2f5808e61c4db69f517acb98964f7e61e
artdian <46709199+jartdian@users.noreply.github.com>
2023-12-06 20:41:11 +0100

    add headers to allow CORS

--- api/index.js
+++ api/index.js
@@ -6,6 +6,13 @@ const citiesLangData = require('../data/citiesLangData.js');
 
 app.use(express.static('public'));
 
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', 'http://127.0.0.1:5500');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    res.header('Access-Control-Allow-Methods', 'GET');
+    next()
+})
+
 app.get('/languages/',(req,res) => {
     res.json(citiesLangData)
 });
